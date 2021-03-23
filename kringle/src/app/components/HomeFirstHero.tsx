d827a6e0b02606e072651d90b9a9924ae5c12b42
James <thejamesash1@gmail.com>
2021-03-23 19:45:33 -0400

    add features comp

--- kringle/src/app/components/HomeFirstHero.tsx
+++ kringle/src/app/components/HomeFirstHero.tsx
@@ -4,11 +4,11 @@ interface HomeFirstHeroProps {}
 
 export const HomeFirstHero : React.FC<HomeFirstHeroProps> = () => {
     return (
-        <div className={'bg-gray-300'}>
+        <div className={'bg-gray-300 pl-6'}>
         <div className="lg:2/6 xl:w-2/4 mt-20 lg:mt-40 lg:ml-16 text-left">
         <div className="text-6xl font-semibold text-gray-900 leading-none">Tinder, for music!</div>
-        <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">Refresh your playlists with music taylored to your ears!</div>
-        <button className="mt-6 px-8 py-4 rounded-full font-normal tracking-wide bg-gradient-to-b from-yellow-600 to-red-700 text-white outline-none focus:outline-none hover:shadow-lg hover:from-blue-700 transition duration-200 ease-in-out">
+        <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">Refresh your playlists with music tailored to your ears!</div>
+        <button className="mt-6 px-8 py-4 rounded-full font-normal tracking-wide bg-gradient-to-b from-yellow-600 to-red-700 text-white outline-none focus:outline-none hover:bg-cool-gray-100 transition duration-200 ease-in-out">
           Download Now
         </button>
       </div>
