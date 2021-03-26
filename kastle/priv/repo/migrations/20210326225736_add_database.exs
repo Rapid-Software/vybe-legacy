99301a30e2b5f0e4ad4232da7e879b0ad0348466
James <thejamesash1@gmail.com>
2021-03-26 19:29:40 -0400

    add db structure

--- kastle/priv/repo/migrations/20210326225736_add_database.exs
+++ kastle/priv/repo/migrations/20210326225736_add_database.exs
@@ -3,5 +3,33 @@ defmodule Data.Repo.Migrations.AddDatabase do
 
   def change do
 
+    create table(:users) do
+      add :uid, :string
+      add :type, :string
+      add :spotify_at, :string
+      add :spotify_rt, :string
+    end
+
+    create table(:songs) do
+      add :sid, :string
+      add :type, :string
+      add :name, :artist
+    end
+
+    create table(:liked_songs) do
+      add :uqid, :string
+      add :uid, references(:users)
+      add :sid, references(:songs)
+    end
+
+    create table(:rejected_songs) do
+      add :uqid, :string
+      add :uid, references(:users)
+      add :sid, references(:songs)
+    end
+
+    create unique_index(:liked_songs, [:uid, :sid])
+    create unique_index(:rejected_songs, [:uid, :sid])
+
   end
 end
