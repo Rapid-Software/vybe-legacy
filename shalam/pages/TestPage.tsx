05d1deb981a659c4bfd0d9c95be9c1533201993f
James <thejamesash1@gmail.com>
2021-03-26 14:32:54 -0400

    add test page

--- shalam/pages/TestPage.tsx
+++ shalam/pages/TestPage.tsx
@@ -1,2 +1,12 @@
 import React from "react";
+import { View, Text } from "react-native";
 
+export const TestPage: React.FC = () => {
+    return (
+        <View>
+        <Text>
+            Test
+        </Text>
+        </View>
+    )
+};
