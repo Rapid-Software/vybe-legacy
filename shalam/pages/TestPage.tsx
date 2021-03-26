978cc74a28417685a39d2561b61f01d66c4ea4c0
James <thejamesash1@gmail.com>
2021-03-26 14:33:43 -0400

    add useNav...()

--- shalam/pages/TestPage.tsx
+++ shalam/pages/TestPage.tsx
@@ -1,7 +1,9 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text } from "react-native";
 
 export const TestPage: React.FC = () => {
+    const navigation = useNavigation();
     return (
         <View>
         <Text>
