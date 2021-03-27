a34d1f67aed234f67789750414b890d266630c53
James <thejamesash1@gmail.com>
2021-03-27 10:33:13 -0400

    (kastle) add user access function defs

--- kastle/lib/data/users.ex
+++ kastle/lib/data/users.ex
@@ -3,6 +3,12 @@ defmodule Data.Users do
     # Access Functions
     defdelegate find_by_uid(id) to: Data.Access.Users
 
+    defdelegate get_all_songs(id) to: Data.Access.Users
+    defdelegate get_liked_songs(id) to: Data.Access.Users
+    defdelegate get_rejected_songs(id) to: Data.Access.Users
+    defdelegate get_spotify_at(id) to: Data.Access.Users
+    defdelegate get_spotify_rt(id) to: Data.Access.Users
+
     # Mutation Functions
     defdelegate t, to: Data.Mutations.Users
 
