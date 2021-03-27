9d36bfbd2478f871c4bd53d2301e7a97b0852f72
James <thejamesash1@gmail.com>
2021-03-27 10:54:26 -0400

    (kastle) add missing commas

--- kastle/lib/data/users.ex
+++ kastle/lib/data/users.ex
@@ -1,14 +1,14 @@
 defmodule Data.Users do
 
     # Access Functions
-    defdelegate find_by_uid(id) to: Data.Access.Users
+    defdelegate find_by_uid(id), to: Data.Access.Users
 
-    defdelegate get_all_songs(id) to: Data.Access.Users
-    defdelegate get_liked_songs(id) to: Data.Access.Users
-    defdelegate get_rejected_songs(id) to: Data.Access.Users
-    
-    defdelegate get_spotify_at(id) to: Data.Access.Users
-    defdelegate get_spotify_rt(id) to: Data.Access.Users
+    defdelegate get_all_songs(id), to: Data.Access.Users
+    defdelegate get_liked_songs(id), to: Data.Access.Users
+    defdelegate get_rejected_songs(id), to: Data.Access.Users
+
+    defdelegate get_spotify_at(id), to: Data.Access.Users
+    defdelegate get_spotify_rt(id), to: Data.Access.Users
 
     # Mutation Functions
     defdelegate edit_spotify_at(id, t), to: Data.Mutations.Users
