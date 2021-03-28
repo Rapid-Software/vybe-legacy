0fcdd0aa17c03e49d88610a96249b1bfa7c618d5
James <thejamesash1@gmail.com>
2021-03-28 17:34:47 -0400

    (kastle) add skeleton to access user functions

--- kastle/lib/data/access/users.ex
+++ kastle/lib/data/access/users.ex
@@ -1,3 +1,27 @@
 defmodule Data.Access.Users do
 
+  def find_by_uid(id) do
+
+  end
+
+  def get_all_songs(id) do
+
+  end
+
+  def get_liked_songs(id) do
+
+  end
+
+  def get_rejected_songs(id) do
+
+  end
+
+  def get_spotify_at(id) do
+
+  end
+
+  def get_spotify_rt(id) do
+
+  end
+
 end
