8abec879a0f12c4a6bd6e10455a6f934799b1fc5
James <thejamesash1@gmail.com>
2021-03-28 15:24:57 -0400

    (kastle) add more db functions for songs

--- kastle/lib/data/songs.ex
+++ kastle/lib/data/songs.ex
@@ -2,10 +2,12 @@ defmodule Data.Songs do
 
     # Access Functions
     defdelegate find_by_uid(sid), to: Data.Access.Songs
+    defdelegate find_by_pid(type, pid), to: Data.Access.Songs
 
     defdelegate find_by_type(type), to: Data.Access.Songs #Type: Spotify/Sc/Apple
     # Mutation Functions
     defdelegate add_song(type, id), to: Data.Mutations.Songs
 
+    defdelegate find_or_create(type, id), to: Data.Mutations.Songs
     # Query Functions
 end
