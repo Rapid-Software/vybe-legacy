00884cf446a23dcba486d44b0662f7e1432f9fde
James <thejamesash1@gmail.com>
2021-03-28 15:15:56 -0400

    (kastle) add migration stuff

--- kastle/priv/repo/migrations/20210328191238_add_platform_id_song.exs
+++ kastle/priv/repo/migrations/20210328191238_add_platform_id_song.exs
@@ -2,6 +2,18 @@ defmodule Data.Repo.Migrations.AddPlatformIdSong do
   use Ecto.Migration
 
   def change do
+    alter table("songs") do
+      add :pid, :string
+    end
 
+    alter table("liked_songs") do
+      add :pid, :string
+      add :type, :string
+    end
+
+    alter table("rejected_songs") do
+      add :pid, :string
+      add :type, :string
+    end
   end
 end
