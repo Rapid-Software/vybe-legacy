209d086b0413aac7b67554a3353b98a856a77e4b
James <thejamesash1@gmail.com>
2021-03-28 15:12:02 -0400

    (kastle) add platform_id to songs and start song db functions

--- kastle/lib/data/songs.ex
+++ kastle/lib/data/songs.ex
@@ -1,8 +1,11 @@
 defmodule Data.Songs do
 
     # Access Functions
+    defdelegate find_by_uid(sid), to: Data.Access.Songs
 
+    defdelegate find_by_type(type), to: Data.Access.Songs #Type: Spotify/Sc/Apple
     # Mutation Functions
-    
+    defdelegate add_song(type, id), to: Data.Mutations.Songs
+
     # Query Functions
 end
