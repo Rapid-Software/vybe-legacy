1cfe37c3db9510b69db982fedc5879fa70cada21
James <thejamesash1@gmail.com>
2021-03-28 21:07:35 -0400

    (kastle) move find_by_spotify_id to access

--- kastle/lib/data/access/users.ex
+++ kastle/lib/data/access/users.ex
@@ -1,7 +1,22 @@
 defmodule Data.Access.Users do
 
+  import Ecto.Query
+
+  alias Data.Schemas.User
+  alias Data.Repo
+
   def find_by_uid(id) do
 
+  end
+
+  def find_by_spotify_id(spotify_id) do
+    t =
+      from(u in User,
+      where:
+      u.spotify_id == ^spotify_id,
+      limit: 1
+      )
+      |> Repo.one()
   end
 
   def get_all_songs(id) do
