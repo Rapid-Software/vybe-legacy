776c71018105a7099e962cfb403f9c9a94ec0409
James <thejamesash1@gmail.com>
2021-03-31 18:00:16 -0400

    (kastle) add find_or_create() code

--- kastle/lib/data/mutations/songs.ex
+++ kastle/lib/data/mutations/songs.ex
@@ -1,11 +1,17 @@
 defmodule Data.Mutations.Songs do
 
   def add_song(type, id, name, artist) do
-
+    {:created, id}
   end
 
   def find_or_create(type, id, name, artist) do
-    t = type |> Data.Access.Songs.find_by_pid(id)
+    {_, t} = type |> Data.Access.Songs.find_by_pid(id)
+
+    if t |> is_nil  do
+      add_song(type, id, name, artist)
+    else
+      {:ok, t}
+    end
   end
 
 end
