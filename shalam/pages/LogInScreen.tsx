dafd0a549495092ef6b7e257131c530caa73b4cb
James <thejamesash1@gmail.com>
2021-04-02 17:52:31 -0400

    (shalam) start converting login page

--- shalam/pages/LogInScreen.tsx
+++ shalam/pages/LogInScreen.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, Text, StyleSheet, ImageBackground } from "react-native";
+import { View, Text, StyleSheet, ImageBackground, Image, TouchableOpacity } from "react-native";
 
 const img = require("../assets/images/bg.png");
 const logo = require("../assets/images/logo.png")
@@ -10,7 +10,10 @@ export const LoginScreen: React.FC = () => {
     return (
         <View style={styles.container}>
             <ImageBackground source={img} style={styles.bg}>
-
+                <Image source={logo} style={styles.logo} />
+                <Text style={styles.headerText}>Hello There!</Text>
+                <Text style={styles.subtitleText}>In order to get started we’re gonna have to make some connections.</Text>
+                <TouchableOpacity style={styles.spotifyButton}>Login with Spotify</TouchableOpacity>
             </ImageBackground>
         </View>
     )
@@ -30,15 +33,42 @@ const styles = StyleSheet.create({
         height: "812px"
     },
     logo: {
-
+        position: "absolute",
+        width: 32,
+        height: 60,
+        left: 30,
+        top: 60
     },
     headerText: {
-
+        position: "absolute",
+        width: 250,
+        height: 38,
+        textAlign: "center",
+        top: 275,
+        fontFamily: "Roboto",
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: 32,
+        lineHeight: 37,
+        color: "#dcdcdc"
     },
     subtitleText: {
-
+        position: "absolute",
+        fontFamily: "Roboto",
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: 12,
+        lineHeight: 16,
+        textAlign: "center",
+        top: 325,
+        color: "#dcdcdc",
+        width: 350
     },
     spotifyButton: {
-        
+        position: "absolute",
+        backgroundColor: "#2ebd59",
+        width: 306,
+        height: 45,
+        borderRadius: 10,
     }
 });
