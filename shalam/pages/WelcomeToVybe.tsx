99fecf0e0f24f5e004652e2f959871e39bcb3dd9
James <thejamesash1@gmail.com>
2021-04-02 13:24:37 -0400

    (shalam) working welcome page

--- shalam/pages/WelcomeToVybe.tsx
+++ shalam/pages/WelcomeToVybe.tsx
@@ -1,15 +1,19 @@
 import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { View, Text, StyleSheet, ImageBackground, Image } from "react-native";
+import { View, Text, StyleSheet, ImageBackground, Image, TouchableOpacity } from "react-native";
 
 const img = require("../assets/images/bg.png");
+const logo = require("../assets/images/logo.png")
 
 export const WelcomeToVybe: React.FC = () => {
     const navigation = useNavigation();
     return (
         <View style={styles.container}>
             <ImageBackground source={img} style={styles.linearGradient}>
-                <Text>Test</Text>
+                <Image source={logo} style={styles.logo}></Image>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Get Started</Text>
+                </TouchableOpacity>
             </ImageBackground>
         </View>
     )
@@ -22,16 +26,53 @@ const styles = StyleSheet.create({
     linearGradient: {
         flex: 1,
         resizeMode: "cover",
-        justifyContent: "center"
+        justifyContent: "center",
+        alignItems: "center",
+        position: "relative",
+        width: "375px",
+        height: "812px"
     },
     logo: {
-
+        position: "absolute",
+        width: "79.18px",
+        height: "145px",
+        left: "148px",
+        top: "301px"
     },
     logoText: {
-
+        position: "absolute",
+        width: "100px",
+        height: "28px",
+        left: "160px",
+        top: "446px",
+        color: "#dcdcdc",
+        fontFamily: "Roboto",
+        fontSize: "24px",
+        lineHeight: "28px",
+        fontWeight: "bold",
+        fontStyle: "normal"
     },
     button: {
-
+        position: "absolute",
+        width: "233px",
+        height: "56px",
+        left: "71px",
+        top: "677px",
+        backgroundColor: "rgba(255,255,255,0.27)",
+        borderRadius: 15,
+        textAlign: "center"
+    },
+    buttonText: {
+        position: "relative",
+        fontFamily: "Roboto",
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: 18,
+        lineHeight: 28,
+        textAlign: "center",
+        top: "13px",
+        textAlignVertical: "center",
+        color: "rgba(255,255,255,0.86)"
     },
     legal: {
 
