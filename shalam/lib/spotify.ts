80fd526087f13f3ecebd1bf2467f6a23c01121ed
James <thejamesash1@gmail.com>
2021-04-02 19:54:14 -0400

    (shalam) handleLogin in spotify done

--- shalam/lib/spotify.ts
+++ shalam/lib/spotify.ts
@@ -2,4 +2,12 @@ import * as auth from "expo-auth-session";
 
 export class SpotifyLogin {
 
+    async handleLogin() : Promise<auth.AuthSessionResult> {
+        let rurl: string = auth.makeRedirectUri();
+        let r: auth.AuthSessionResult = await auth.startAsync({
+            authUrl: ""
+        });
+        return r;
+    }
+
 }
