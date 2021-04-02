e7a61df3b18fe54cddb7e56723af2f89486c1c76
James <thejamesash1@gmail.com>
2021-04-02 14:27:00 -0400

    (shalam) add todo in App.tsx

--- shalam/App.tsx
+++ shalam/App.tsx
@@ -2,6 +2,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { MainNav } from "./navigators/MainNav";
 
+// todo: store login creds (spotify id // etc / token)
+// : mainNav ? welcome nav
+
 export default function App() {
   return (
     <NavigationContainer>
