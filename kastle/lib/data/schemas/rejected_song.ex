2831df44c0c4da547aa25a5d56b39642bf744bb4
James <thejamesash1@gmail.com>
2021-04-02 23:50:02 -0400

    (kastle) fix schemas

--- kastle/lib/data/schemas/rejected_song.ex
+++ kastle/lib/data/schemas/rejected_song.ex
@@ -2,8 +2,11 @@ defmodule Data.Schemas.RejectedSong do
   use Ecto.Schema
 
   schema "rejected_songs" do
+    field :uqid, :string
     field :uid, :string
     field :sid, :string
+    field :type, :string
+    field :pid, :string
   end
 
 end
