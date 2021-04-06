13bd2e156181ae16b1fda9a3baacae49685af1b6
James <thejamesash1@gmail.com>
2021-04-05 22:44:46 -0400

    (shalam) add useQueueStore

--- shalam/pages/MainApp.tsx
+++ shalam/pages/MainApp.tsx
@@ -3,38 +3,17 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Swipeable } from "react-native-gesture-handler";
 import { SongCard, SongCardObject } from "../components/SongCard";
-
-export const QueueContext = React.createContext<{ q: SongCardObject[]}>({
-    q: []
-});
-
-const onSwipeRight = () => {
-    return (
-        <Text>
-        right
-    </Text>
-    )
-};
-
-const onSwipeLeft = () => {
-    return (
-        <Text>
-        left
-    </Text>
-    )
-};
+import { useQueueStore, addQueue } from "../stores/useQueueStore";
 
 export const MainApp: React.FC = () => {
     const navigation = useNavigation();
+    const s = useQueueStore().queue;
 
     return (
     <View style={styles.container}>
-            <Swipeable renderRightActions={onSwipeRight}
-            renderLeftActions={onSwipeLeft}>
             <Text>
-                this text is moveable
+                MAIN APP
             </Text>
-            </Swipeable>
     </View>
     )
 };
