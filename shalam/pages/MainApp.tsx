dd9632b12e48e77bfc12822a7478bd3d4a82b264
James <thejamesash1@gmail.com>
2021-04-05 21:06:54 -0400

    (shalam) start working on main app

--- shalam/pages/MainApp.tsx
+++ shalam/pages/MainApp.tsx
@@ -1,23 +1,44 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, {useState} from "react";
 import { View, Text, StyleSheet } from "react-native";
-import GestureRecognizer from 'react-native-swipe-gestures';
-import swipeDirections from "react-native-swipe-gestures";
+import { Swipeable } from "react-native-gesture-handler";
+
+const onSwipeRight = () => {
+    return (
+        <Text>
+        right
+    </Text>
+    )
+};
+
+const onSwipeLeft = () => {
+    return (
+        <Text>
+        left
+    </Text>
+    )
+};
 
 export const MainApp: React.FC = () => {
     const navigation = useNavigation();
+
     return (
-        <View style={styles.container}>
-        <Text>
-            Test
-        </Text>
-        </View>
+    <View style={styles.container}>
+            <Swipeable renderRightActions={onSwipeRight}
+            renderLeftActions={onSwipeLeft}>
+            <Text>
+                this text is moveable
+            </Text>
+            </Swipeable>
+    </View>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#1b1b1d"
+        backgroundColor: "#1b1b1d",
+        alignItems: "center",
+        justifyContent: "center"
     },
 });
