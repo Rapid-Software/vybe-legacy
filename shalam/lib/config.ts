0222fae5c7113f0e297855517b0d39a4da807760
James <thejamesash1@gmail.com>
2021-04-07 17:16:48 -0400

    (shalam) switch const to config

--- shalam/lib/config.ts
+++ shalam/lib/config.ts
@@ -1,4 +1,8 @@
 export const cfg = {
     apiEndpoint: "https://gateway.vybemusic.app",
-    websocketEndpoint: "wss://gateway.vybemusic.app/socket"
+    websocketEndpoint: "wss://gateway.vybemusic.app/socket",
+    WSSettings: {
+        connectionTimeout: 20000,
+        heartbeatInterval: 30000
+    }
 };
