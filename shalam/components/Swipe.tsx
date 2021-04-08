06e603f4bd3e7e80197a68472bdcf9077cc5de85
James <thejamesash1@gmail.com>
2021-04-07 22:01:12 -0400

    (shalam) start swiper comp

--- shalam/components/Swipe.tsx
+++ shalam/components/Swipe.tsx
@@ -1,10 +1,20 @@
-import React from "react";
-import { TempSongCard } from "../components/TempSongCard";
+import React, { useState, useContext } from "react";
+import { TempSongCard, QueueContext, QueueSongInfo } from "../components/TempSongCard";
+import Swiper from "react-native-deck-swiper";
 
+const renderNextCard = () => {
+    
+}
 
 export const Swipe: React.FC = () => {
+    const { list } = useContext(QueueContext); 
+    const [index, setIndex] = useState(0);
+
     return (
-        <TempSongCard>
-        </TempSongCard>
-    )
+        <Swiper
+        cards={list}
+        cardIndex={index}
+        renderCard={(card)=><TempSongCard info={card} />}
+        />
+    );
 }
