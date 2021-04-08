ae4ca9d0b509738d8f4061cbb9ffe28e19416c6f
James <thejamesash1@gmail.com>
2021-04-07 22:08:11 -0400

    (shalam) continue swiper

--- shalam/components/Swipe.tsx
+++ shalam/components/Swipe.tsx
@@ -3,18 +3,38 @@ import { TempSongCard, QueueContext, QueueSongInfo } from "../components/TempSon
 import Swiper from "react-native-deck-swiper";
 
 const renderNextCard = () => {
-    
+
+}
+
+const onSwipeRight = (index: number) => {
+
+}
+
+const onSwipeLeft = (index: number) => {
+
 }
 
 export const Swipe: React.FC = () => {
     const { list } = useContext(QueueContext); 
     const [index, setIndex] = useState(0);
 
+    const onSwipe = () => {
+        setIndex(index + 1);
+    }    
+
     return (
         <Swiper
         cards={list}
         cardIndex={index}
         renderCard={(card)=><TempSongCard info={card} />}
+        onSwiped={onSwipe}
+        stackSize={2}
+        stackScale={10}
+        stackSeparation={14}
+        disableTopSwipe
+        disableBottomSwipe
+        onSwipedRight={(index)=>onSwipeRight(index)}
+        onSwipedLeft={(index)=>onSwipeLeft(index)}
         />
     );
 }
