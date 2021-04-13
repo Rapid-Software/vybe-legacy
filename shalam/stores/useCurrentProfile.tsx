ead7d4a82567a33056b48e37eff814e39bd6e039
jamesql <thejamesash1@gmail.com>
2021-04-13 11:53:37 -0400

    (shalam) start working on current profile context

--- shalam/stores/useCurrentProfile.tsx
+++ shalam/stores/useCurrentProfile.tsx
@@ -1,1 +1,23 @@
-export default null;
+import React, { useContext } from "react";
+import { ProfileInfo } from "../types";
+import { QueueSongInfo } from "../components/TempSongCard"
+
+export const NullProfile : ProfileInfo = {
+    username: "",
+    followers: 0,
+    following: 0,
+    description: "",
+    likedSongs: [],
+    profilePicture: ""
+}
+
+export const ProfileContext = React.createContext<{ user: ProfileInfo }>({
+    user: NullProfile
+});
+
+export const useCurrentProfile = () => {
+    const { user } = useContext(ProfileContext);
+
+    
+
+};
