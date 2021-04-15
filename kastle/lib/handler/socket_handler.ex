c61abe2b56a10cec4a5c15ed6a84116b9a5e89df
jamesql <thejamesash1@gmail.com>
2021-04-15 19:54:28 -0400

    (kastle) add test song for socket

--- kastle/lib/handler/socket_handler.ex
+++ kastle/lib/handler/socket_handler.ex
@@ -108,7 +108,17 @@ defmodule Handler.SocketHandler do
     end
 
     def handler("get_new_songs", %{}, state) do
-        {:reply, make_socket_msg(%{"op" => "get_new_songs_done", "d" => %{}}), state}
+        songs = %{
+           %{
+              "songName" => "Bruh Random Song",
+              "platform" => "spotify",
+              "pid" => "some rando id",
+              "image" => "https://i.scdn.co/image/ab67616d0000b273e1f2015c50b29afa49f5c296",
+              "artist" => "Bruh Man",
+              "playbackUrl" => "https://p.scdn.co/mp3-preview/a18df55248609b3165d63e145fc75ad5c309be09?cid=774b29d4f13844c495f206cafdad9c86"
+           }
+        }
+        {:reply, make_socket_msg(%{"op" => "get_new_songs_done", "d" => songs}), state}
     end
 
     def handler("get_user_profile", %{"username" => uname}, state) do # finish
