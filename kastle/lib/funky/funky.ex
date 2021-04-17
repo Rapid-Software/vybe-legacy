5dfc241e8a1325bbb734c3ba4ab2fbf23d8f2d34
jamesql <thejamesash1@gmail.com>
2021-04-17 17:21:44 -0400

    (kastle) start algo

--- kastle/lib/funky/funky.ex
+++ kastle/lib/funky/funky.ex
@@ -4,6 +4,14 @@ defmodule Funky do
 
   end
 
+  def get_spotify_suggestion(limit, genres, artists, songs) do
+
+  end
+
+  def get_random_hip_hop_song(limit) do
+
+  end
+
   def convert_spotify_to_song() do
 
   end
