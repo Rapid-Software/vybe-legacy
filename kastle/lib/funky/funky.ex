d0210c96b06348cbe31bac865fd67eee70e2e5df
jamesql <thejamesash1@gmail.com>
2021-04-22 12:28:24 -0400

    (kastle) add spotify reco

--- kastle/lib/funky/funky.ex
+++ kastle/lib/funky/funky.ex
@@ -9,11 +9,19 @@ defmodule Funky do
   end
 
   def get_random_hip_hop_song(limit) do
-
+    creds = "0de489a8-ba81-423f-b1c3-847508a4f85e" |> create_connection()
+    {:ok, r} = creds |> Spotify.get_recommendations(market: "US", limit: limit, seed_genres: "hip-hop")
+    IO.inspect(r)
   end
 
   def convert_spotify_to_song() do
 
+  end
+
+  def create_spotify_connection(token) do
+    {:ok, u} = Data.Access.Users.tokens_to_user()
+
+    %Spotify.Credentials{"access_token" => u.spotify_at, "refresh_token" => u.spotify_rt}
   end
 
 end
