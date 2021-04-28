92241ab46c3239b9a92388cc46a23171f185891f
jamesql <thejamesash1@gmail.com>
2021-04-28 11:10:02 -0400

    (kastle) debug again

--- kastle/lib/funky/spotify_api.ex
+++ kastle/lib/funky/spotify_api.ex
@@ -45,9 +45,7 @@ defmodule Spotty do
     {:ok, u} = token |> Data.Access.Users.tokens_to_user()
     {:ok, r} = refresh_post("https://accounts.spotify.com/api/token", u.spotify_rt)
 
-    IO.inspect(r)
-    IO.puts("\nbreak\n\n")
-    IO.inspect(r.body)
+    IO.inspect(r.body |> Poison.decode())
 
     case r.body |> Poison.decode() do
       %{"access_token" => token, "token_type" => type, "scope" => scope, "expires_in" => expires, "refresh_token" => refresh_token} ->
